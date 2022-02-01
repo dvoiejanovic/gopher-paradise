ea049f4ed6c643e41fd4de5559af36129393d5a9
Ivo Dejanovic <ivo.dejanovic@productive.io>
2022-02-01 08:45:55 +0100

    interfaces

---
+++ interfaces.go
@@ -0,0 +1,43 @@
+/*
+	An interface type is defined as set of method signatures.
+	A value of interface type can hold any value that implements these methods.
+*/
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Abser interface {
+	Abs() float64
+}
+
+func main() {
+	var a Abser
+	f := MyFloat(-math.Sqrt2)
+	v := Vertex{3, 4}
+
+	a = f  // a MyFloat implements Abser
+	a = &v //a *Vertex implements Abser
+
+	fmt.Println(a.Abs())
+}
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
