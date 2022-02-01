022c498aa53739a2e6f01d791f0f1ead1241a6b4
Ivo Dejanovic <ivo.dejanovic@productive.io>
2022-02-01 08:45:38 +0100

    broken code

---
+++ web-service-gin/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Album represents data about a record album
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+// albums slice to seed record album data.
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+	c.indentedJson(http.StatusOK, albums)
+}
+
+// gin.Context is the most important part of Gin. It carries request details, validates and serializes JSON, and more.
+// Call Context.IndentedJSON to serialize the struct into JSON and add it to the response.
