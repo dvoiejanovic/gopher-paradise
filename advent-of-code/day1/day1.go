81f88580cf1cd68cb7be0e4670a9ed4a6b0aa91e
Ivo Dejanovic <ivo.dejanovic@productive.io>
2022-02-03 21:43:20 +0100

    day 1 finishedgit add .

--- advent-of-code/day1/day1.go
+++ advent-of-code/day1/day1.go
@@ -3,14 +3,36 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"strconv"
+	"strings"
 )
 
 func main() {
-	depths, _ := ioutil.ReadFile("input.txt")
-	// depthIncrements := 0
-	// for _, depth := range depths {
-	// 	fmt.Println(depth)
-	// }
-	fmt.Println(strconv.Atoi(int(depths)))
+	content, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		log.Fatalf("unable to read file: %v", err)
+	}
+
+	depths := strings.Fields(string(content))
+	depthIncrements := 0
+
+	for i, _ := range depths {
+		if i == 0 {
+			continue
+		}
+
+		previousDepthInt, _ := strconv.Atoi(depths[i-1])
+		depthInt, err := strconv.Atoi(depths[i])
+
+		if err != nil {
+			log.Fatalf("Can't convert to int")
+			break
+		}
+
+		if depthInt > previousDepthInt {
+			depthIncrements++
+		}
+	}
+	fmt.Println(depthIncrements)
 }
