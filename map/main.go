ddf4f6171a505bc1958e02b7043d5176e79183ef
Ivo Dejanovic <ivo.dejanovic@productive.io>
2022-02-03 21:00:49 +0100

    go maps

---
+++ map/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println(color, hex)
+	}
+
+}
+
+func main() {
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"blue":  "#0000ff",
+		"white": "#ffffff",
+	}
+
+	printMap(colors)
+
+}
